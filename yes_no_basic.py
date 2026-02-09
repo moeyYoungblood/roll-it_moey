09e5d1723750214208304d23376a24a9375f115a
moeyYoungblood <alim50568@masseyhigh.school.nz>
2026-02-10 10:15:36 +1300

    simple yes / no checker (incomplete)

---
+++ yes_no_basic.py
@@ -0,0 +1,11 @@
+want_instructions=input("Does Selu like kava?"). lower()
+
+# check the user sayas yes / no
+if want_instructions == "yes" or want_instructions == "y":
+    print ("you said yes")
+elif want_instructions == "no" or want_instructions == "n":
+    print ("you said no")
+
+else:
+    print ("say yes / no")
+
